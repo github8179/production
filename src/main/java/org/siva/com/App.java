47a768790fd604c9f74ed15e7741872e502e2ecc
root <root@control>
2018-07-26 04:32:19 +0000

    maven pom.xml file

---
+++ src/main/java/org/siva/com/App.java
@@ -0,0 +1,13 @@
+package org.siva.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
